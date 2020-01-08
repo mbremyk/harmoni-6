1b3ddc4422cce8578148b4903baec765d85234e7
MarTorb <marius.torbjornsen@gmail.com>
2020-01-08 15:47:16 +0100

    Add dropdown for artists

--- public/src/components/createevent.js
+++ public/src/components/createevent.js
@@ -4,7 +4,54 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import DateTimePicker from 'react-datetime-picker';
 import Card from "react-bootstrap/Card";
+import Dropdown from "react-bootstrap/Dropdown";
+import FormControl from "react-bootstrap/FormControl";
+import {Button} from "react-bootstrap";
+
 export class addEvent extends Component{
+
+    CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+        <a
+            href=""
+            ref={ref}
+            onClick={e => {
+                e.preventDefault();
+                onClick(e);
+            }}
+        >
+            {children}
+            &#x25bc;
+        </a>
+    ));
+
+    CustomMenu = React.forwardRef(
+        ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
+            const [value, setValue] = React.useState('');
+
+            return (
+                <div
+                    ref={ref}
+                    style={style}
+                    className={className}
+                    aria-labelledby={labeledBy}
+                >
+                    <FormControl
+                        autoFocus
+                        className="mx-3 my-2 w-auto"
+                        placeholder="Type to filter..."
+                        onChange={e => setValue(e.target.value)}
+                        value={value}
+                    />
+                    <ul className="list-unstyled">
+                        {React.Children.toArray(children).filter(
+                            child =>
+                                !value || child.props.children.toLowerCase().startsWith(value),
+                        )}
+                    </ul>
+                </div>
+            );
+        },
+    );
 
     state = {
         date: new Date(),
@@ -43,10 +90,28 @@ export class addEvent extends Component{
                             </div>
                         <Form.Label column={1}>Aldersgrense</Form.Label>
 
+                        <Dropdown>
+                            <Dropdown.Toggle as={this.CustomToggle} id="dropdown" onSelect={this.save()}>
+                                Custom toggle
+                            </Dropdown.Toggle>
 
+                            <Dropdown.Menu as={this.CustomMenu}>
+                                <Dropdown.Item eventKey="1">Red</Dropdown.Item>
+                                <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
+                                <Dropdown.Item eventKey="3" active>
+                                    Orange
+                                </Dropdown.Item>
+                                <Dropdown.Item eventKey="1">Red-Orange</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
                     </Form>
                 </Card>
             </Container>
         );
+    }
+
+    save() {
+        console.log("tset");
+        console.log(document.getElementById("dropdown"));
     }
 }
