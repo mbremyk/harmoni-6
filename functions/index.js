63d1ce0e3b751edcd692f7e230132879e39f1ff6
magbre <magbre@stud.ntnu.no>
2020-01-08 12:46:17 +0100

    Get requests for fetching all concerts and concerts fulfilling search criteria

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,12 @@
-import Sequelize from 'sequelize';
-import { compare, hash } from 'bcrypt';
-import {sign, verify} from "jsonwebtoken";Object.defineProperty(exports, "__esModule", {value: true});
+Object.defineProperty(exports, "__esModule", {value: true});
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
 const bodyParser = require("body-parser");
 const model = require('./model.js');
 model.syncModels();
+const sequelize = require("sequelize");
+const op = sequelize.Op;
 let cors = require("cors");
 
 admin.initializeApp(functions.config().firebase);
@@ -23,4 +23,22 @@ app.get("/test", (req, res) => {
     console.log(req);
     res.send("test functional");
 });
+
+app.get("/concerts", (req, res) => {
+    console.log("GET-request received from client");
+    return model.ConcertModel.findAll({order: [['dateTime', 'ASC']]})
+        .then(concerts => res.send(concerts))
+        .catch(error => console.error(error));
+});
+
+app.get("/concerts/search/:searchText", (req, res) => {
+    console.log("GET-request received from client");
+    return model.ConcertModel.findAll({
+        where: {[op.or]: [{concertName: {[op.like]: `%${req.params.searchText}%`}}, {description: {[op.like]: `%${req.params.searchText}%`}}]},
+        order: [['dateTime', 'ASC']]
+    })
+        .then(concerts => res.send(concerts))
+        .catch(error => console.error(error));
+});
+
 console.log("Server initalized");
