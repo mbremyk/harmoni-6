3a75603c786028dbb213b5a77996fdd038b4a145
Jakob <42341254+JLMadsen@users.noreply.github.com>
2020-01-09 10:44:16 +0100

    removed boilerplate test

--- public/src/index.js
+++ public/src/index.js
@@ -14,29 +14,6 @@ import {myPage} from "./components/mypage.js";
 const history = createBrowserHistory();
 const url = "http://localhost:5001/harmoni-6/us-central1/webApi/api/v1/";
 
-class Test extends Component{
-    render(){
-        return(
-            <div>
-                <button onClick={this.test}>Test webapp</button>
-            </div>
-        );
-    }
-    test(){
-        console.log("Test called");
-        fetch(url+"test", {
-            method: "GET",
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8',
-                'x-access-token': 'undefined',
-                "Access-Control-Allow-Origin": "true"
-            }
-        })
-            .then(res => alert(res.json()))
-            .catch(err => alert(err));
-    }
-}
-
 const root = document.getElementById('root');
 if (root)
     ReactDOM.render(
