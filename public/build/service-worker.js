e231f32e7c0be6443225063ba884aa3fa3e4c054
Steffen Trømborg <steffetr@stud.ntnu.no>
2020-01-14 10:42:00 +0100

    /min-side update username and email now works. Does not as of now check if email and username already exists. When /min-side is mounted the correct username and email is now displayed in the form.

--- public/build/service-worker.js
+++ public/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b47300aca57d1fef96970a27b68361a2.js"
+  "/precache-manifest.e2b3683e3b37d0f324ad468d3ca37e4d.js"
 );
 
 self.addEventListener('message', (event) => {
