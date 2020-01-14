8a513b936a0d684341def9eb43b665bf68cd724e
sivertut <sivertut@stud.ntnu.no>
2020-01-14 11:24:29 +0100

    Fixed gigs

--- functions/tests/modelTest.js
+++ functions/tests/modelTest.js
@@ -3,7 +3,8 @@ const dao = require('../dao.js');
 let db = new dao();
 
 
-beforeAll(() => Models.syncTestData().then(() => function(){}));
+beforeAll(() => Models.syncTestData().then(() => function () {
+}));
 
 
 /*
@@ -122,7 +123,6 @@ describe('Users', () => {
 /*
                     EVENTS
  */
-
 
 
 describe('Events', () => {
@@ -307,10 +307,10 @@ describe('Gigs', () => {
     });
 
     it('correct data in gig', done => {
-        db.getGigs(4).then(gig => {
-            expect(gig.artistId).toBe(8);
-            done();
+        db.getGigs(4).then(gigs => {
+          expect(gigs.length).toBe(1);
         });
+        done();
     });
 });
 
