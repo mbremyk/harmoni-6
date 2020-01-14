e3abf38a8fc9da29b038bea7fbe2c65a3f652664
sivertut <sivertut@stud.ntnu.no>
2020-01-14 09:07:59 +0100

    RUN THE COMMANDS GIVEN TO NOT COMMIT CHANGED TO THIS FILE

--- functions/properties.js
+++ functions/properties.js
@@ -1,11 +1,29 @@
+
+/*
+	RUN THIS COMMAND TO IGNORE CHANGES TO THIS FILE
+
+ 	git update-index --skip-worktree ./functions/properties.js
+
+	DO NOT COMMIT CHANGES TO THIS FILE
+ */
+
+
+
 class Properties
 {
-	constructor(){}
+	constructor()
+	{
+	}
 }
+
+
 
 class TestProperties
 {
-	constructor(){}
+	constructor()
+	{
+	}
 }
+
 
 module.exports = {Properties, TestProperties};
