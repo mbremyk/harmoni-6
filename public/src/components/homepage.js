16dce1ece0d601eefea63a672fd4f11987f942c4
mokkalokka <michael.larsen90@gmail.com>
2020-01-15 09:47:45 +0100

    Code cleanup

--- public/src/components/homepage.js
+++ public/src/components/homepage.js
@@ -1,105 +1,62 @@
 import {Component} from "react-simplified";
-import {Container, Row, Col, Button, Form, Alert} from "react-bootstrap";
-import {EventInfo} from '../widgets.js';
-import {createHashHistory} from 'history';
+import {Row, Col} from "react-bootstrap";
 import * as React from 'react';
-import {Event, service, Ticket} from '../services';
+import {service} from '../services';
 import {authService} from '../AuthService'
 import {SortedEventView} from "./sortedeventview";
+
 const jwt = require("jsonwebtoken");
 
-
 export class HomePage extends Component {
-	myEvents = [];
-	allEvents = [];
-	otherEvents = false;
+    myEvents = [];
+    allEvents = [];
 
 
-	/*
-     <Container>
-     <Row>
-     <Col>
+    render() {
+        return (
+            <div>
+                <Row>
+                    <Col md={{span: 12, offset: 4}}>
+                        <h1>Mine Arrangementer</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <SortedEventView myEvent={true} events={this.myEvents}/>
 
-     <Form.Control placeholder="Søk etter arragement"/>
+                </Row>
+                <Row>
+                    <Col md={{span: 12, offset: 4}}>
+                        <h1>Andre Arrangementer</h1>
+                    </Col>
+                </Row>
+                <SortedEventView events={this.getOtherEvents()}/>
+            </div>
+        );
+    }
 
+    mounted() {
+        let token = jwt.decode(authService.getToken());
 
-     </Col>
-     </Row>
+        service
+            .getEventsByOrganizer(token.userId)
+            .then(myEvents => (this.myEvents = myEvents))
+            .catch((error) => console.log(error));
+        this.getAllEvents();
 
-     </Container>
-     */
+    }
 
-	render() {
-			return (
+    //gets all the events in the database and gives the array allEvents this value
+    getAllEvents() {
+        service
+            .getEvents()
+            .then(otherEvents => (this.allEvents = otherEvents))
+            .catch((error) => console.log(error));
+    }
 
-
-
-					<div>
-
-					<Row>
-						<Col md={{span: 12, offset: 4}}>
-							<h1>Mine Arrangementer</h1>
-						</Col>
-					</Row>
-
-
-					<Row>
-						<SortedEventView myEvent={true} events={this.myEvents}/>
-
-					</Row>
-
-
-					<Row>
-						<Col md={{span: 12, offset: 4}}>
-							<h1>Andre Arrangementer</h1>
-						</Col>
-					</Row>
-
-					<SortedEventView events={this.getOtherEvents()}/>
-
-					</div>
-
-
-
-			);
-
-	}
-
-
-	mounted() {
-		let token = jwt.decode(authService.getToken());
-
-		service
-			.getEventsByOrganizer(token.userId)
-			.then(myEvents => (this.myEvents = myEvents))
-			.catch((error) => console.log(error));
-
-		this.getAllEvents();
-
-
-
-
-	}
-
-	//gets all the events in the database and gives the array allEvents this value
-	getAllEvents() {
-		service
-			.getEvents()
-			.then(otherEvents => (this.allEvents = otherEvents))
-			.catch((error) => console.log(error));
-
-	}
-
-	//checks if the event is organized by the logged in user, if not it goes in this array
-	getOtherEvents()
-	{
-		let token = jwt.decode(authService.getToken());
-		let otherEvents = this.allEvents.filter(e => e.organizerId !== token.userId);
-		return otherEvents;
-
-	}
-
-
-
-
+    //checks if the event is organized by the logged in user, if not it goes in this array
+    getOtherEvents() {
+        let token = jwt.decode(authService.getToken());
+        let otherEvents = this.allEvents.filter(e => e.organizerId !== token.userId);
+        return otherEvents;
+    }
 }
