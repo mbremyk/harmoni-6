f3d4994806f1e6d083fcf2d7269b5564ce935f77
Jakob <42341254+JLMadsen@users.noreply.github.com>
2020-01-15 11:21:08 +0100

    css

--- public/src/components/footer.js
+++ public/src/components/footer.js
@@ -7,18 +7,20 @@ export class Footer extends Component{
 
 	render() {
 		return (
-			<div className="bg-secondary text-center" style={{height: "100px"}}>
-				<CardColumns>
+			<div className="-fill">
+				<div className="bg-secondary text-center align-bottom" style={{height: "100px"}}>
+					<CardColumns>
 
-					<p className="text-white font-italic">Harmoni</p>
+						<p className="text-white font-italic">Harmoni</p>
 
-					<p className="text-white">
-						Kontakt oss her: <NavLink href="https://www.facebook.com/Marius.Torbjornsen">
-						<u className="text-white">Facebook</u></NavLink>
-					</p>
+						<p className="text-white">
+							Kontakt oss her: <NavLink href="https://www.facebook.com/Marius.Torbjornsen">
+							<u className="text-white">Facebook</u></NavLink>
+						</p>
 
-				</CardColumns>
+					</CardColumns>
 
+				</div>
 			</div>
 		);
 	}
