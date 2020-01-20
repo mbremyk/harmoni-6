a513e0842ae8aaa1475d6ce7e83954fb88f3ae19
Jakob <42341254+JLMadsen@users.noreply.github.com>
2020-01-20 12:35:09 +0100

    check if email belongs to a user

--- functions/dao.js
+++ functions/dao.js
@@ -117,6 +117,9 @@ class Dao {
     }
 
     async forgotPassword(email) {
+        let result = await this.getUserByEmail(req.params.email);
+        if(result === null) { console.log('No email found for temp pass.'); return; }
+
         let newPass = Math.random().toString(36).substring(7);
         let salt = await this.getSaltByEmail(email);
         let credentials = await hashPassword.hashPassword(newPass, salt[0].dataValues.salt);
