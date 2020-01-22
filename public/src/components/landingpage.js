76dc07b02bf14c9d1562a1d7b3952aa44d4e040e
mokkalokka <michael.larsen90@gmail.com>
2020-01-22 15:03:14 +0100

    Mobilvennlig UI

--- public/src/components/landingpage.js
+++ public/src/components/landingpage.js
@@ -6,70 +6,72 @@ import {Event, service, Ticket} from '../services';
 import NavLink from "react-bootstrap/NavLink";
 
 export class LandingPage extends Component {
-	state = {
-		sortOption: {field: '', order: 'asc'},
-		events: []
-	};
+    state = {
+        sortOption: {field: '', order: 'asc'},
+        events: []
+    };
 
-	tickets = [];
-	searchevents = [];
+    tickets = [];
+    searchevents = [];
 
-	render() {
-		return (
-			<div>
-				<div className="landingpage">
-					<Container>
-						<Row style={{padding: "5em"}}>
-							<Col>
-								<h1 className="HarmoniLogo display-sm-3 text-center">Harmoni</h1>
-							</Col>
-						</Row>
-						<Row style={{padding: "5em "}}>
-							<Col className="text-center">
-								<h2><NavLink href="/logg-inn"><u className="text-white">Logg inn</u></NavLink> for å se dine arrangementer</h2>
-							</Col>
-						</Row>
-						<Row style={{padding: "5em"}}>
-							<Col className="text-center mt-5">
-								<NavLink href='#scrollTo'>
-									<h2 className="text-white"><u>Bla ned</u> for å se arrangementer</h2>
-									<i className="fa fa-angle-down" style={{'font-size':'64px',color: 'white'}}></i>
-								</NavLink>
-							</Col>
-						</Row>
-					</Container>
+    render() {
+        return (
+            <div>
+                <div className={"landingpage"}>
+
+                        <Row style={{padding: "5em"}}>
+                            <Col>
+                                <h1 className="HarmoniLogo display-sm-3 text-center">Harmoni</h1>
+                            </Col>
+                        </Row>
+                        <Row style={{padding: "5em "}}>
+                            <Col className="text-center">
+                                <h2><NavLink href="/logg-inn"><u className="text-white">Logg inn</u></NavLink> for å se
+                                    dine arrangementer</h2>
+                            </Col>
+                        </Row>
+                        <Row style={{padding: "5em"}}>
+                            <Col className="text-center mt-5">
+                                <NavLink href='#scrollTo'>
+                                    <h2 className="text-white"><u>Bla ned</u> for å se arrangementer</h2>
+                                    <i className="fa fa-angle-down" style={{'font-size': '64px', color: 'white'}}></i>
+                                </NavLink>
+                            </Col>
+                        </Row>
+                </div>
+				<div>
+					<SortedEventView/>
 				</div>
-				<SortedEventView/>
 			</div>
-		);
-	}
+        );
+    }
 
-	mounted() {
-		service
-			.getEvents()
-			.then(events => (this.handleEvents(events)))
-			.catch((error) => console.log(error));
-	}
+    mounted() {
+        service
+            .getEvents()
+            .then(events => (this.handleEvents(events)))
+            .catch((error) => console.log(error));
+    }
 
-	getTicketPrice(eventId) {
-		service
-			.getTicketToEvent(eventId)
-			.then(tickets => {
-				this.tickets = tickets;
-				let lowestPrice = tickets[0].price;
-				for (let i = 0; i < tickets.length; i++) {
-					if (tickets[i].price < lowestPrice) {
-						lowestPrice = tickets[i].price;
-						console.log(lowestPrice);
-					}
-				}
-				return lowestPrice;
-			})
-			.catch((error) => console.log(error));
-	}
+    getTicketPrice(eventId) {
+        service
+            .getTicketToEvent(eventId)
+            .then(tickets => {
+                this.tickets = tickets;
+                let lowestPrice = tickets[0].price;
+                for (let i = 0; i < tickets.length; i++) {
+                    if (tickets[i].price < lowestPrice) {
+                        lowestPrice = tickets[i].price;
+                        console.log(lowestPrice);
+                    }
+                }
+                return lowestPrice;
+            })
+            .catch((error) => console.log(error));
+    }
 
-	scroll() {
-		let element = document.getElementById('scrollTo');
-		element.scrollIntoView({behavior: "smooth"});
-	}
+    scroll() {
+        let element = document.getElementById('scrollTo');
+        element.scrollIntoView({behavior: "smooth"});
+    }
 }
