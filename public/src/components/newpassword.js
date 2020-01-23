0f594df14dbec920c25a95a56a189160a0f0c901
mokkalokka <michael.larsen90@gmail.com>
2020-01-23 15:34:21 +0100

    Fixed padding bottom footer

--- public/src/components/newpassword.js
+++ public/src/components/newpassword.js
@@ -45,7 +45,7 @@ export class NewPassword extends Component {
 
 	render() {
 		return(
-			<Container className={"c-sm"} style={{top: '0'}}>
+			<Container className={"c-sm"} style={{top: '0' , paddingBottom: '15em'}}>
 
 				<h1 className="HarmoniLogo display-sm-3 text-center m-5">Harmoni</h1>
 
