252fa4ea5c1c065cef9f12bb217c5885cde18dd6
sivertut <sivertut@stud.ntnu.no>
2020-01-24 14:42:57 +0100

    Removing default 'info' in description

--- public/src/components/eventpage.js
+++ public/src/components/eventpage.js
@@ -255,10 +255,10 @@ export class EventPage extends Component {
 
     emailForm() {
         if (this.artists.length != 0 && this.isOrganizer) {
-            return <MailForm hasRecipients={true} description={"Info"} recipients={this.artists.concat(this.personnel)}
+            return <MailForm hasRecipients={true} recipients={this.artists.concat(this.personnel)}
                              toggleable={true}/>
         } else if (this.currentEvent.eventName && (this.isPersonnel || this.isArtist) && !this.isOrganizer) {
-            return <MailForm hasRecipients={true} description={"Info"}
+            return <MailForm hasRecipients={true}
                              recipients={[{user: this.user}]}
                              toggleable={true}/>
         } else {
