cca82847d40498752bfbca0bd738bc405c037627
magbre <magbre@stud.ntnu.no>
2020-01-24 18:15:43 +0100

    Test test

--- functions/tests/defaultMailTest.js
+++ functions/tests/defaultMailTest.js
@@ -34,6 +34,18 @@ describe('Test of DefaultMail', () => {
             `På 2020-01-01 skrev du: \n\ntext\n\n` +
             `Hvis du har spørsmål eller mer informasjon kan du sende det som svar på denne mailen\n` +
             `\nMed vennlig hilsen\nHarmoni team 6 support team`);
+        expect(dm.passwordText).toBe(`Hei, user\n\n` +
+            `Vi har mottatt en forespørsel om nytt passord fra din bruker. Hvis du ikke har sendt en forespørsel om nytt passord, kan du se bort ifra denne mailen.\n\n` +
+            `Ditt engangspassord er: password\n\n` +
+            `Dette passordet virker kun én gang, og det anbefales å bytte passord på Min Side så fort du har logget inn.\n` +
+            `\nMed vennlig hilsen\nHarmoni team 6 support team`);
+        expect(dm.contactText).toBe(`Hei, user\n\n` +
+            `Vi har mottatt din epost og ser på den så fort vi har tid.\n\n` +
+            `Ditt support-nummer er 123456\n\n` +
+            `På 2020-01-01 skrev du: \n\ntext\n\n` +
+            `Hvis du har flere spørsmål eller informasjon som er relevant for denne saken kan du sende det som et svar på denne mailen\n` +
+            `\nMed vennlig hilsen\nHarmoni team 6 support team`);
+        expect(dm.infoText).toBe(`Den 2020-01-01 skrev user:\n\ntext\n\nuser kan nåes på email`);
         done();
     })
 });
